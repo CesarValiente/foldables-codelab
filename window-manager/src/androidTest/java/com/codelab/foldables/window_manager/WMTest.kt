5a656798764fc4f53b81b6745b2e872cc1acb633
Cesar Valiente <cevalien@microsoft.com>
2021-11-17 21:59:51 +0100

    Tests updated to latest wm version

--- window-manager/src/androidTest/java/com/codelab/foldables/window_manager/WMTest.kt
+++ window-manager/src/androidTest/java/com/codelab/foldables/window_manager/WMTest.kt
@@ -1,19 +1,19 @@
 package com.codelab.foldables.window_manager
 
-import android.graphics.Rect
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
+import androidx.test.espresso.assertion.PositionAssertions.isCompletelyLeftOf
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.window.layout.DisplayFeature
-import androidx.window.layout.WindowInfoRepository.Companion.windowInfoRepository
-import androidx.window.layout.WindowLayoutInfo
+import androidx.window.layout.FoldingFeature.Orientation.Companion.HORIZONTAL
+import androidx.window.layout.FoldingFeature.Orientation.Companion.VERTICAL
+import androidx.window.layout.FoldingFeature.State.Companion.FLAT
+import androidx.window.testing.layout.FoldingFeature
+import androidx.window.testing.layout.TestWindowLayoutInfo
 import androidx.window.testing.layout.WindowLayoutInfoPublisherRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toCollection
-import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class WMTest {
 
-    private val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private val activityRule = ActivityScenarioRule(CodelabActivity::class.java)
     private val publisherRule = WindowLayoutInfoPublisherRule()
 
     @get:Rule
@@ -36,26 +36,41 @@ class WMTest {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun test_that_WindowInfoRepo_has_the_correct_info() {
-        runBlockingTest {
-            val hinge = object : DisplayFeature {
-                override val bounds: Rect
-                    get() = Rect(1350, 0, 1434, 1800)
-            }
-            val expected = WindowLayoutInfo.Builder().setDisplayFeatures(listOf(hinge)).build()
-            activityRule.scenario.onActivity { activity ->
-                val values = mutableListOf<WindowLayoutInfo>()
-                val value = async {
-                    activity.windowInfoRepository().windowLayoutInfo.take(1).toCollection(values)
-                }
-                publisherRule.overrideWindowLayoutInfo(expected)
-                runBlockingTest {
-                    assertEquals(
-                        listOf(expected),
-                        value.await().toList()
-                    )
-                }
-            }
+    fun testText_is_left_of_Vertical_FoldingFeature() {
+        activityRule.scenario.onActivity { activity ->
+            val hinge = FoldingFeature(
+                activity = activity,
+                state = FLAT,
+                orientation = VERTICAL,
+                size = 2
+            )
+
+            val expected = TestWindowLayoutInfo(listOf(hinge))
+            publisherRule.overrideWindowLayoutInfo(expected)
         }
+
+        onView(withId(R.id.layout_change)).check(
+            isCompletelyLeftOf(withId(R.id.device_feature))
+        )
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testText_is_below_FoldingFeature() {
+        activityRule.scenario.onActivity { activity ->
+            val hinge = FoldingFeature(
+                activity = activity,
+                state = FLAT,
+                orientation = HORIZONTAL,
+                size = 2
+            )
+
+            val expected = TestWindowLayoutInfo(listOf(hinge))
+            publisherRule.overrideWindowLayoutInfo(expected)
+        }
+
+        onView(withId(R.id.layout_change)).check(
+            isCompletelyBelow(withId(R.id.device_feature))
+        )
     }
 }
