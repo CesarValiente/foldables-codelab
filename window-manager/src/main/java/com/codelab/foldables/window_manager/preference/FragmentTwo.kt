5106d65a5bc0d271a996bcb055f7360fd54457b0
Cesar Valiente <cevalien@microsoft.com>
2021-12-16 17:57:13 +0100

    Added detail panes

---
+++ window-manager/src/main/java/com/codelab/foldables/window_manager/preference/FragmentTwo.kt
@@ -0,0 +1,15 @@
+package com.codelab.foldables.window_manager.preference
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codelab.foldables.window_manager.R
+
+class FragmentTwo : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+}
