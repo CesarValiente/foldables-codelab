3655857670476c2f83877b7d8ec26735bb371964
Cesar Valiente Gordo <cevalien@microsoft.com>
2022-06-07 15:54:04 +0200

    Cevalien/wm 1 1 0 alpha01 (#12)
    * Using 1.1.0-alpha01 dependency
    
    * Updated kotlin version
    
    * Updated split rules and use new enums for finishXXX fields
    
    * Added buttons and logic to know wheter an activity is embedded or not
    
    * Brought stable codelab code as well

--- window-manager/src/main/java/com/codelab/foldables/window_manager/embedding/ActivityC.kt
+++ window-manager/src/main/java/com/codelab/foldables/window_manager/embedding/ActivityC.kt
@@ -2,7 +2,9 @@ package com.codelab.foldables.window_manager.embedding
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.window.embedding.SplitController
 import com.codelab.foldables.window_manager.databinding.EmbeddedActivityCBinding
 
 class ActivityC : AppCompatActivity() {
@@ -17,6 +19,10 @@ class ActivityC : AppCompatActivity() {
         binding.launchActivityB.setOnClickListener {
             val intent = Intent(this, ActivityB::class.java)
             startActivity(intent)
+        }
+        binding.isActivityEmbedded.setOnClickListener {
+            val isActivityEmbedded = SplitController.getInstance().isActivityEmbedded(this)
+            Toast.makeText(this, "activity embedded? $isActivityEmbedded", Toast.LENGTH_SHORT).show()
         }
     }
 }
